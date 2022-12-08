c4adf38a014a6d9c14cfd20645404189002d0d60
Iwuchukwu Ikechukwu <iwuchukwuonline@gmail.com>
2022-12-08 12:29:31 -0800

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/*This is my first C Programming code. The program will print string using the function PUTS and return value 0 */
+/**
+ *main - empty
+ *value 0
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");

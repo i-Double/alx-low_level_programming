9663d5543ddce8e25e877ef885b12c4152505f28
Iwuchukwu Ikechukwu <iwuchukwuonline@gmail.com>
2022-12-09 14:33:04 -0800

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,14 +4,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)  
+int main(void)
 {
 char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 int i;
-for (i = 0; i < 26; i++)    
-{      
-putchar(alp[i]);      
-}  
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i]);
+}
 putchar('\n');
 return (0);
 }

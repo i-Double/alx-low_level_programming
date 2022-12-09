f06763c27f8b73746babf401bab1a77a793812f0
Iwuchukwu Ikechukwu <iwuchukwuonline@gmail.com>
2022-12-09 09:02:44 -0800

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,14 +9,14 @@
  */
 int main(void)
 {
-  int n;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  if (n > 0)
-    printf("%i is possible\n", n);
-  else if (n < 0)
-    printf("%i is negative\n", n);
-  else
-    printf("%i is zero\n", n);
-  return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%i is possible\n", n);
+else if (n < 0)
+printf("%i is negative\n", n);
+else
+printf("%i is zero\n", n);
+return (0);
 }

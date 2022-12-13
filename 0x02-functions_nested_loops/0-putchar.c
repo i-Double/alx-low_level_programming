a13ed4a956416c54fd91a3f6004d1ae34013f96a
Iwuchukwu Ikechukwu <iwuchukwuonline@gmail.com>
2022-12-13 05:54:37 -0800

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - This will print _putchar
+*Return: 0
+*/
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - this program prints _putchar
+*
+*/
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}

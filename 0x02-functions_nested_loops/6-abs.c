f06b427ca9ad7d85d3d65e2d4a76dc6308b631ad
Iwuchukwu Ikechukwu <iwuchukwuonline@gmail.com>
2022-12-13 14:03:44 -0800

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - always retun positve value
+ * @t: the integer
+ * Return: the absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}

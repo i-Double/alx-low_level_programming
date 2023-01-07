603bbae3354983728f470b0e95deacb93a400c69
Iwuchukwu Ikechukwu <iwuchukwuonline@gmail.com>
2023-01-06 19:37:04 -0800

    retry

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,6 +1,6 @@
 #include "main.h"
+#include <stdio.h>
 void _puts(char *str)
-#include<stdio.h>
 /**
  * main - Entry point
  *
@@ -8,7 +8,7 @@ void _puts(char *str)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\"");
 	return (0);
 }
 

d23571c7e9636da2a72edd454c2b19b5d44ec499
Iwuchukwu Ikechukwu <iwuchukwuonline@gmail.com>
2023-01-16 12:14:48 -0800

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,20 +2,21 @@
 #define DOG_H
 
 
-	/**
-	 * struct dog - a dog's basic info
-	 * @name: First member
-	 * @age: Second member
-	 * @owner: Third member
-	 *
-	 * Description: Longer description
-	 */
-	struct dog
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
+/**
+* Description: - struct dog - a dog's basic info
+* @name: First member
+* @age: Second member
+* @owner: Third member
+* Description: - Longer description
+*/
+
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+};
 
 
 	/**
@@ -32,4 +33,4 @@
 	int _strlen(char *s);
 
 
-	#endif
+#endif
